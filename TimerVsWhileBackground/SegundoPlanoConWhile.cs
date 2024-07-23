91759b824a6f1ea964459a5405b9615c0a289d9e
Felipe Gavilan <elmejor_887@hotmail.com>
2024-07-23 19:47:10 -0400

    initial commit

---
+++ TimerVsWhileBackground/SegundoPlanoConWhile.cs
@@ -0,0 +1,25 @@
+﻿
+namespace TimerVsWhileBackground
+{
+    public class SegundoPlanoConWhile : BackgroundService
+    {
+        int instanciasSiendoEjecutadas = 0;
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                instanciasSiendoEjecutadas++;
+                Console.WriteLine($"WHILE: comenzando ejecución... (Tareas siendo ejecutadas {instanciasSiendoEjecutadas})");
+
+                // Simulamos una tarea larga
+                await Task.Delay(TimeSpan.FromSeconds(10));
+
+                Console.WriteLine($"WHILE ({DateTime.Now.ToString("hh:mm:ss")}): Ejecutando tarea en segundo plano...");
+                instanciasSiendoEjecutadas--;
+
+                await Task.Delay(TimeSpan.FromSeconds(2), stoppingToken);
+
+            }
+        }
+    }
+}
---
+++ TimerVsWhileBackground/SegundoPlanoTimer.cs
@@ -0,0 +1,36 @@
+﻿
+namespace TimerVsWhileBackground
+{
+    public class SegundoPlanoTimer : BackgroundService
+    {
+        private Timer timer;
+        private int instanciasSiendoEjecutadas = 0;
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            timer = new Timer(EjecutarTarea, null, TimeSpan.Zero, TimeSpan.FromSeconds(2));
+            return Task.CompletedTask;
+        }
+
+        private async void EjecutarTarea(object? state)
+        {
+            Interlocked.Add(ref instanciasSiendoEjecutadas, 1);
+
+            Console.WriteLine($"TIMER: comenzando ejecución... (Tareas siendo ejecutadas {instanciasSiendoEjecutadas})");
+
+            // Simulamos una tarea larga
+            await Task.Delay(TimeSpan.FromSeconds(10));
+
+            Interlocked.Add(ref instanciasSiendoEjecutadas, -1);
+
+            Console.WriteLine($"TIMER ({DateTime.Now.ToString("hh:mm:ss")}): Ejecutando tarea en segundo plano...");
+        }
+
+        public override void Dispose()
+        {
+            timer?.Dispose();
+            base.Dispose();
+        }
+
+    }
+}
